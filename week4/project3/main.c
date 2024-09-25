e58c8685b5c8f49cc87038a905da2d2e1076d660
Hatjeans <julia0634@sookmyung.ac.kr>
2024-09-25 14:00:33 +0900

    week4
    week4

---
+++ week4/project3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char character1, character2;
+		
+	printf("enter a character : ");
+	scanf("%c", &character1 );
+	
+	printf("the next character of %c (%d) is %c (%d) ", character1, character1, character1 +1, character1 +1  );
+	
+	return 0;
+}
